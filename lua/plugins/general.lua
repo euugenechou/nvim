3667de7b8bc04f0da1db3776d03f0da5895e2f49
Eugene Chou <euchou@ucsc.edu>
2023-02-21 22:27:07 -0800

    mlep

---
+++ lua/plugins/general.lua
@@ -0,0 +1,6 @@
+return {
+  { "tpope/vim-eunuch" },
+  { "tpope/vim-repeat" },
+  { "tpope/vim-surround" },
+  { "tpope/vim-unimpaired" },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "rhysd/vim-clang-format",
+    config = function()
+      vim.g["clang_format#detect_style_file"] = 1
+      vim.g["clang_format#auto_format"] = 1
+      vim.g["clang_format#enable_fallback_style"] = 0
+    end,
+  },
+  {
     "simrat39/rust-tools.nvim",
     ft = { "rust", "rs" },
     keys = {

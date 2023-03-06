74198f75b06025a7db65ea52fb85785cd75db79a
Eugene Chou <euchou@ucsc.edu>
2023-03-05 22:51:52 -0800

    more autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,22 @@
+-- strip whitespace
 vim.api.nvim_create_autocmd({ "BufWritePre", "FileWritePre" }, {
-  command = "%s/\\s\\+$//e",
+    command = "%s/\\s\\+$//e",
 })
 
+-- the proper comment style for C/C++
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp" },
-  command = "setlocal commentstring=//\\ %s",
+    pattern = { "c", "cpp" },
+    command = "setlocal commentstring=//\\ %s",
+})
+
+-- highlight yanks
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+-- check if file should be reloaded
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+    command = "checktime",
 })

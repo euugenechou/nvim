3a30316866eb60d1bc8d306685fcb3c1a9bca94d
Eugene Chou <euchou@ucsc.edu>
2023-03-09 14:57:53 -0800

    git treesitter stuff

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -4,6 +4,8 @@ require('nvim-treesitter.configs').setup {
   ensure_installed = {
     'bash',
     'cpp',
+    'gitcommit',
+    'gitignore',
     'haskell',
     'help',
     'lua',
@@ -48,20 +50,20 @@ require('nvim-treesitter.configs').setup {
       enable = true,
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
       },
       goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
       },
       goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
       },
       goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
       },
     },
     swap = {

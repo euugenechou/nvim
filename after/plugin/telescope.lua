331edc43297cbe155a78f3fbb0596e11345b2600
Eugene Chou <euchou@ucsc.edu>
2023-05-11 12:57:01 +0200

    cleaner telescope ui

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -36,6 +36,28 @@ require('telescope').setup {
         end,
       },
     },
+    prompt_prefix = "   ",
+    selection_caret = "  ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "ascending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        preview_width = 0.55,
+        results_width = 0.8,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
+    winblend = 0,
+    border = {},
+    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
   },
 }
 
